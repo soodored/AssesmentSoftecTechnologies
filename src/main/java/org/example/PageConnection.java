0a1c79e86af2bbb09923a82ab084ee3c152b3e04
stanislavaleshkevich <soodoStas@gmail.com>
2020-06-29 23:29:08 +0300

    Code refactoring

--- src/main/java/org/example/PageConnection.java
+++ src/main/java/org/example/PageConnection.java
@@ -7,15 +7,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 public class PageConnection {
-    public Document getPageByUrl(String url){
+    public Document getPageByUrl(String url) {
 
         Document document = null;
         try {
@@ -23,24 +20,13 @@ public class PageConnection {
                 BufferedReader fileReader = new BufferedReader(new FileReader(new File(new URL(url).toURI())));
                 String page = fileReader.lines().collect(Collectors.joining());
                 document = Jsoup.parse(page);
-            }
-            else {
+            } else {
                 document = Jsoup.connect(url).get();
             }
         } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
         return document;
-    }
-
-    boolean isValid(String url) {
-        try {
-            URL validUrl = new URL(url);
-            return true;
-        }
-        catch (Exception e) {
-            return false;
-        }
     }
 
 }
